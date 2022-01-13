4006f6c3b5e75b00b3c0bbdde426d6f184d4c800
Murat B.Çelebi <73561693+uWozy@users.noreply.github.com>
2022-01-13 14:28:36 +0300

    Update

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,7 @@
 let result = document.getElementById("inputCurrent")
-let operation = ["*", "-", "+", "÷",]
+let operation = ["*", "-", "+", "/",]
 
 let calculate=(number)=>{
-
   if (result.innerHTML == 0) {
     result.innerHTML = ""
   }
@@ -12,14 +11,22 @@ let calculate=(number)=>{
   result.innerText +=number;
 }
 
+let oper=(number)=>{
+  if (result.innerHTML >= 1) {
+    for(let i = 3; i < operation.length; i++) {
+      result.innerHTML += number;
+    }
+  }
+}
+
 function reverseNum() {
   // incomplete code
-  result.innerHTML = result.innerHTML * -1
+ // result.innerHTML = result.innerHTML * -1
 }
 function clearInput() {
   result.innerHTML = 0;
 }
-
 function processEnd() {
   result.innerText = eval(result.innerHTML)
 }
+
